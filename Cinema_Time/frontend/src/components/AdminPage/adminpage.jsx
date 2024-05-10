a8dcde75eddc00fd553661ca2f71aea47badf517
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-05-10 11:19:22 +0300

    items done! excpted anything depends on id front&back

--- Cinema_Time/frontend/src/components/AdminPage/adminpage.jsx
+++ Cinema_Time/frontend/src/components/AdminPage/adminpage.jsx
@@ -6,14 +6,14 @@ import { Outlet } from "react-router-dom";
 function AdminPage() {
     return (
         <>
-            <div className="vh-100 adminbg position-relative">
-                <div className="container-fluid position-absolute z-1 py-5">
-                    <div className="my-2 d-flex position-fixed">
+            <div className="vh-100 adminbg">
+                <div className="vh-100 overflow-auto bluradmin-bg">
+                    <div className="my-5 d-flex position-fixed">
                         <div className="my-2">  <Sidenav /> </div>
                         <p className="text-warning p-2 my-2 h2">DashBoard</p>
                     </div>
 
-                    <div className="container-fluid py-4 mt-4">
+                    <div className="container-fluid py-4 mt-5">
                         <Counters />
                     </div>
 
