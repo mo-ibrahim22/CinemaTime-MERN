1e3f844ab9db0cc4be03aa7a55873026515b12dc
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-05-11 11:05:50 +0300

    make animation for about

--- Cinema_Time/frontend/src/components/AboutPage/OurPartners.jsx
+++ Cinema_Time/frontend/src/components/AboutPage/OurPartners.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImdb, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faPlay, faGhost } from '@fortawesome/free-solid-svg-icons';
 
 const OurPartners = () => {
     return (
         <div>
             <h3 className="h3 text-warning">Our Partners</h3>
             <ul className="text-light">
-                <li>Youtube</li>
-                <li>IMDb</li>
-                <li>JustWatch</li>
-                <li>WitAnime</li>
+                <li> <FontAwesomeIcon icon={faYoutube} /> Youtube</li>
+                <li> <FontAwesomeIcon icon={faImdb} /> IMDb</li>
+                <li> <FontAwesomeIcon icon={faPlay} /> JustWatch</li>
+                <li> <FontAwesomeIcon icon={faGhost} /> WitAnime</li>
             </ul>
         </div>
     );
